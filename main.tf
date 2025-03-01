a4ce8a533f349846dc3ba0f6e39408fac6bee56d
Mohammad Aakhil <149184615+Mraakhil@users.noreply.github.com>
2025-03-02 02:46:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,7 @@
 resource "aws_instance" "public_instance" {
  ami           = var.ami
  instance_type = var.instance_type
- key_name = "newAccount"
- key_name = "newAccount"
+
 
  tags = {
    Name = var.name_tag,
